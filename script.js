c3ebc286c5a14b413c0add3377d842d294955c82
tasibnat2706 <tasibnat2706@gmail.com>
2026-03-16 15:21:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
-async function showResults() {
+function goToResults(){
 
 const id = document.getElementById("participantId").value.trim();
-const resultsDiv = document.getElementById("results");
 
-if (!id) {
-resultsDiv.style.display = "block";
-resultsDiv.innerHTML = "<p>Please enter an ID.</p>";
+if(!id){
+alert("Please enter your ID");
 return;
 }
 
-try {
-
-const response = await fetch("data.json");
-const data = await response.json();
-
-if (data[id]) {
-
-resultsDiv.style.display = "block";
-
-resultsDiv.innerHTML = `
-<h3>Hello ${data[id].name}</h3>
-<p><strong>Summary:</strong> ${data[id].summary}</p>
-<p><strong>Heart Rate:</strong> ${data[id].data.heart_rate}</p>
-<p><strong>Blood Pressure:</strong> ${data[id].data.blood_pressure}</p>
-<p><strong>Interpretation:</strong> ${data[id].interpretation}</p>
-`;
-
-} else {
-
-resultsDiv.style.display = "block";
-resultsDiv.innerHTML = "<p>ID not found. Please check and try again.</p>";
-
-}
-
-} catch (error) {
-
-resultsDiv.style.display = "block";
-resultsDiv.innerHTML = "<p>Error loading data.</p>";
-console.error(error);
-
-}
+window.location.href = "results.html?id=" + id;
 
 }
